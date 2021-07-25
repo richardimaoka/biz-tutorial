482dd666c11fe6b49eaafcd08be19b722bafa1b5
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-07-25 14:40:46 +0900

    return data from json

--- server/index.ts
+++ server/index.ts
@@ -6,14 +6,29 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
-  // ...other resolver definitions...
+  Query: {
+    page(parent: any, args: any, context: any, info: any) {
+      if (args.id === 11988) {
+        return context.data_11988;
+      } else {
+        null;
+      }
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   mocks: true,
-  mockEntireSchema: true,
+  mockEntireSchema: false,
+  context: async ({ req }: any) => {
+    const data = fs.readFileSync(
+      __dirname.concat("/qwiklabs-11988.json"),
+      "utf8"
+    );
+    return { data_11988: JSON.parse(data) };
+  },
 });
 
 // The `listen` method launches a web server.

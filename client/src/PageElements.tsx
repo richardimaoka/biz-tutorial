53a623c1228fa4bbd686e57a0fdb8a056ef7d7a9
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-03 08:49:15 +0900

    let it compile

--- client/src/PageElements.tsx
+++ client/src/PageElements.tsx
@@ -5,7 +5,7 @@ import { Video } from "./Video";
 export const PageElements = () => (
   <div>
     <PageTitle title="概要:cypressはフロントエンドend-to-endテストの代表格、WSLで動かしてみよう！" />
-    <Paragraph />
+    {/* <Paragraph /> */}
     <Video />
   </div>
 );

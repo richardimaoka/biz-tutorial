daa51ef932a5becdc7ae1ee5baefa10b04d72d66
Richard Imaoka <richard.e.imaoka@gmail.com>
2021-11-03 15:52:11 +0900

    query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,56 @@
 //import { css } from "@emotion/react";
 import { HeaderContainer } from "./HeaderContainer";
 import { MainContainer } from "./MainContainer";
-import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
+import {
+  InMemoryCache,
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from "@apollo/client";
+import React from "react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
+const query = gql`
+  query {
+    tutorial {
+      title
+      pages {
+        title
+        elements {
+          ... on Paragraph {
+            body
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Internal = (): JSX.Element => {
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) {
+    return <div>{"Loading..."}</div>;
+  } else if (error) {
+    return <div>{`Error! ${error.message}`}</div>;
+  } else {
+    return (
+      <React.Fragment>
+        <HeaderContainer title={data.tutorial.title}></HeaderContainer>
+        <MainContainer></MainContainer>
+      </React.Fragment>
+    );
+  }
+};
+
 const App = (): JSX.Element => (
   <ApolloProvider client={client}>
-    <HeaderContainer title="CypressをWSL2上で動かすために必要な作業"></HeaderContainer>
-    <MainContainer></MainContainer>
+    <Internal />
   </ApolloProvider>
 );
 
